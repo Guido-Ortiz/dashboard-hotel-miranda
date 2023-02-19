1a2a73ba3965f2ef2b8475db174b7065d5ab9cd8
Guido Ortiz <guidoortiz93@hotmail.com>
2023-02-19 15:07:08 +0100

    sidebar active path

--- src/components/Sidebar/SidebarItem/SidebarItem.jsx
+++ src/components/Sidebar/SidebarItem/SidebarItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { ItemWrapper } from './SidebarItemStyles'
 
 const SidebarItem = ({ icon, item, link }) => {
+    const location = useLocation()
+    // console.log(location);
     return (
         <Link to={link} style={{ textDecoration: 'none' }}>
-            <ItemWrapper>
+            <ItemWrapper location={location.pathname} link={link}>
                 {icon}
                 <h4>{item}</h4>
             </ItemWrapper>
--- src/components/Sidebar/SidebarItem/SidebarItemStyles.js
+++ src/components/Sidebar/SidebarItem/SidebarItemStyles.js
@@ -12,26 +12,21 @@ export const ItemWrapper = styled.div`
     padding-left: 20px;
     margin: 5px 0;
 
+    border-left: ${props => props.location === props.link ? '3px solid #E23428' : null} ;
+    
     svg {
-        margin-right: 20px
-       
+        margin-right: 20px;
+        color: ${props => props.location === props.link ? '#E23428' : '#799283'};
     }
 
     h4 {
         text-align: left;
         font: normal normal normal 18px/27px Poppins;
         letter-spacing: 0px;
-        color: #799283;
+        color: ${props => props.location === props.link ? '#E23428' : '#799283'};
         opacity: 1;
     }
-    &:active {
-        color: #E23428;
-        border-left: 3px solid #E23428;
-    }
-    &:focus {
-        color: #E23428;
-        border-left: 3px solid #E23428;
-    }
+    
     &:hover {
         color: #E23428;
         border-left: 3px solid #E23428;
