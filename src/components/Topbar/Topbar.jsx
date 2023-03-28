c686cc62d5bfba9ffac772d182387738f1a8453c
Guido Ortiz <guidoortiz93@hotmail.com>
2023-03-28 16:07:40 +0200

    logo link

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
 import Logo from './Logo/Logo';
 import { Wrapper } from './TopbarStyles';
 
 const Topbar = ({ location }) => {
-  
+
   const title = location.split('/')
-  
+
   return (
     <Wrapper location={location}>
+      <Link to='/'>
         <Logo />
-        <Header location={title[1]} />
+      </Link>
+      <Header location={title[1]} />
     </Wrapper>
   )
 }
